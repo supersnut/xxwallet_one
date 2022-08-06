8e52530e59a41890b00cf69f7b51865cb684a92d
supersnut <supersnut@proton.me>
2022-08-06 13:17:50 +0200

    more links

--- components/content/LinkNavbar.vue
+++ components/content/LinkNavbar.vue
@@ -33,11 +33,39 @@ const props = defineProps({
         <nuxt-link to="https://xxnetwork.wiki" class="navbar-item">
           Wiki
         </nuxt-link>
+      </div>
+    <div class="navbar-brand">
+      <nuxt-link to="https://discord.com/invite/Y8pCkbK" class="navbar-item">
+        <img class="social_icon" src="/discord.svg"/>
+      </nuxt-link>
+      <nuxt-link to="https://t.me/xxnetwork" class="navbar-item">
+        <img class="social_icon" src="/telegram.svg"/>
+      </nuxt-link>
+      <nuxt-link to="https://twitter.com/xx_network" class="navbar-item">
+        <img class="social_icon" src="/twitter.svg"/>
+      </nuxt-link>
+      <nuxt-link to="https://coinmarketcap.com/currencies/xx-network/" class="navbar-item">
+        <img class="social_icon" src="/coinmarketcap.png"/>&nbsp;Market
+      </nuxt-link>
+      <nuxt-link to="https://www.mexc.com/exchange/XX_USDT" class="navbar-item">
+        MEXC
+      </nuxt-link>
+      <nuxt-link to="https://forum.xxnetwork" class="navbar-item">
+        Forum
+      </nuxt-link>
+      <nuxt-link to="https://xxnetwork.wiki/How_to_Nominate" class="navbar-item">
+        Staking
+      </nuxt-link>
+
     </div>
   </nav>
 </template>
 
 <style>
+.social_icon {
+  width: 16px;
+  height: 16px;
+}
 .navbar {
   background: rgba(255,255,255,0.85);
   min-height: 2.5rem;
